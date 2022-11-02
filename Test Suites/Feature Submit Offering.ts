2c540eb19223ea1a11209ac64278f7eb3d10130f
tyuniar <yuniartiambarsari@gmail.com>
2022-11-02 17:46:12 +0700

    [tri] submit offering test suites

---
+++ Test Suites/Feature Submit Offering.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Feature Submit Offering</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>194f30d3-012a-41c0-8e16-173292f99dde</testSuiteGuid>
+   <testCaseLink>
+      <guid>6be19a6e-c877-471d-88a2-a60c03c468b2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/StepDefinition/Feature Submit Offering/SUBOF001 - Offering with input invalid Harga Tawar</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0892c71b-6e48-4020-879e-63129a6b6d20</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/StepDefinition/Feature Submit Offering/SUBOF002 - Offering with input valid harga tawar</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
